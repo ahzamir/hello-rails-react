71a054d426df85904a0dcde1d4e929bb53ee5e6f
Ahmad Zamir Yousufi <ahmadzamiryousofi1397@gmail.com>
2022-09-26 23:42:40 +0430

    created the databse schema and created the 5 greetings messages

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Greetings.create([message: "Hello World!"])
+Greetings.create([message: "Bonjour le monde!"])
+Greetings.create([message: "Hola Mundo!"])
+Greetings.create([message: "سلام دنیا!"])
+Greetings.create([message: "Ciao mondo!"])
